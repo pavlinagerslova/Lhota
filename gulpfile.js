c91f241e4357ba44c3311fe0107e6952d57bd437
Pavlína Geršlová <pavlinagerslova@seznam.cz>
2022-11-09 20:30:38 +0100

    Lhota

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+function style () {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+}
+
+exports.style = style;
